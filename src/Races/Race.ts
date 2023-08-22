82cbb06a3ebac1e8d1afe7754c89f80269650a6f
devRenatoRodrigues <rehrlz@hotmail.com>
2023-08-22 15:32:43 -0300

    feat: adding abstract class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+  
+  get name() : string {
+    return this._name;
+  }
+  
+  get dexterity() : number {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints() : number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
