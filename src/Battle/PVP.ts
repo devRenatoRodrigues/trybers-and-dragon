7abd4cdf719612f158a52514412bdf05c86a9cf2
devRenatoRodrigues <rehrlz@hotmail.com>
2023-08-23 13:35:06 -0300

    feat: class pvp and DIP princiope apply to interfaces

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,29 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(
+    player1: Fighter,
+    player2: Fighter,
+  ) {
+    super(player1);
+    this._player2 = player2;
+    this._player1 = player1;
+  }
+
+  fight(): number {
+    while (this._player1.lifePoints > -1 && this._player2.lifePoints > -1) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+    if (this._player1.lifePoints <= -1 && this._player2.lifePoints > -1) {
+      return -1; 
+    } 
+    return 1;
+  }
+}
+
+export default PVP;
